170eee61c5ee44892c7dd92e42a2b9ad042308ad
Daiki Ihara <sasurau4@gmail.com>
2021-02-10 00:22:28 +0900

    Implement ll parser

--- 9ccs.c
+++ 9ccs.c
@@ -20,6 +20,23 @@ struct Token {
     char *str;
 };
 
+typedef enum {
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+    NodeKind kind;
+    Node *lhs;
+    Node *rhs;
+    int val;
+};
+
 /**
  * Globals
  * */
@@ -39,6 +56,9 @@ void error_at(char *loc, char *fmt, ...) {
     exit(1);
 }
 
+/**
+ * Tokenizer
+ * */
 bool consume(char op) {
     if (token->kind != TK_RESERVED || token->str[0] != op)
         return false;
@@ -103,6 +123,62 @@ Token *tokenize(char *p) {
 
     new_token(TK_EOF, cur, p);
     return head.next;
+}
+
+/**
+ * Parser
+ * */
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *primary() {
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    return nde_node_num(expect_number());
+}
+
+Node *mul() {
+    Node *node = primary();
+
+    for(;;) {
+        if (consume('*')) {
+            node = new_node(ND_MUL, node, primary());
+        } else if (consume('/')) {
+            node = new_node(ND_DIV, node, primary());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *expr() {
+    Node *node = mul();
+
+    for (;;) {
+        if (consume('+')) {
+            node = new_node(ND_ADD, node, mul());
+        } else if(consume('-')) {
+            node = new_node(ND_SUB, node, mul());
+        } else {
+            return node;
+        }
+    }
 }
 
 int main(int argc, char **argv) {
