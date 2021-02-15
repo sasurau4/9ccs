342dbdc4d92849483a7338f1869969a60962cd2e
Daiki Ihara <sasurau4@gmail.com>
2021-02-15 22:43:27 +0900

    Implement gen func

--- 9ccs.c
+++ 9ccs.c
@@ -181,6 +181,41 @@ Node *expr() {
     }
 }
 
+void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("    push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("    pop rdi\n");
+    printf("    pop rax\n");
+
+    switch (node->kind) {
+        case ND_ADD: {
+            printf("    add rax, rdi\n");
+            break;
+        }
+        case ND_SUB: {
+            printf("    sub rax, rdi\n");
+            break;
+        }
+        case ND_MUL: {
+            printf("    imul rax, rdi\n");
+            break;
+        }
+        case ND_DIV: {
+            printf("    cqo\n");
+            printf("    idiv rdi\n");
+            break;
+        }
+    }
+
+    printf("    push rax\n");
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr, "The number of arguments is incorrect.\n");
