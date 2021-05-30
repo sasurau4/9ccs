9e5d4256bd38b98cac94a43cf9adfa581302ea33
Daiki Ihara <sasurau4@gmail.com>
2021-05-31 00:21:49 +0900

    Update debug

--- test/debug.c
+++ test/debug.c
@@ -1,8 +1,27 @@
-int test(int a, int b) {
-    return a + b;
+int case_array_2() {
+    // int a[2];*(a+1)=2;return *(a+0);
+    static int a[1];*a=2;return *a;
 }
+
+// int case_ptr_plus_2() {
+//     // From test_func
+//     int *p = alloc1(3,6);
+//     return *(p+1);
+// } 
+
+// int piyo1() {
+//     // int index = 0;
+//     char a[1]; a[0]=10; return a[0];
+// }
+// int piyo2() {
+//     return 15;
+// }
 int main() {
-    int p;
-    p = test(2, 1+1);
-    return p;
+    // return 3;
+    // char a[1]; a[1]=10; a[0];
+    // return piyo1();
+    // int a[2];*(a+1)=2;return *(a+0);
+    // int a[2];*a=2;return *a;
+    // return case_ptr_plus_2();
+    return case_array_2();
 }
